b59932715fc8e3a43e26fb63dd53bc0ee3771cd7
Marston Ward <marston.s.ward@gmail.com>
2026-03-12 21:41:15 -0500

    Fix PortfolioImage component - add use client directive for React hooks

--- src/components/PortfolioImage.tsx
+++ src/components/PortfolioImage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from 'next/image';
 import { useState } from 'react';
 import { getOptimizedImageSet, getPlaceholderImage, type ImageCategory, type ImageType, type ImageSize } from '@/lib/images';
